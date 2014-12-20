4e68bffe125bafec3459db33caaa591225d61520
hjydzh <452034476@qq.com>
2014-12-21 00:40:50 +0800

    s

--- src/dao/intf/ArticleQueryIntf.php
+++ src/dao/intf/ArticleQueryIntf.php
@@ -1,8 +1,12 @@
 <?php
-
 namespace dao\intf;
 
-interface ArticleQueryIntf {
+interface ArticleQueryIntf extends BaseIntf{
+	
+	/*
+	 * 根据目录id获取所有目录
+	 */
+	function getBlogByCatId($catId);
 }
 
 ?>
