f4c68f8182af85dbcc296cdd855461e35525aac7
hjydzh <452034476@qq.com>
2016-04-10 00:22:27 +0800

    修改时间格式

--- public/portal/detail.php
+++ public/portal/detail.php
@@ -29,7 +29,7 @@
 function time_formater(time){
 	 var date = new Date(time*1000);
 	 var M = (date.getMonth()+1 < 10 ? '0'+(date.getMonth()+1) : date.getMonth()+1) + '-';
-	 var D = date.getDate() < 10 ? '0'+date.getDate()+ ':':date.getDate()+ ':';
+	 var D = date.getDate() < 10 ? '0'+date.getDate()+ ' ':date.getDate()+ ' ';
 	 var h = date.getHours() < 10 ? '0'+date.getHours()+ ':':date.getHours()+ ':';
 	 var m = date.getMinutes() < 10 ? '0'+date.getMinutes()+ ':':date.getMinutes()+ ':';
 	 var s = date.getSeconds() < 10 ? '0'+date.getSeconds():date.getSeconds();
